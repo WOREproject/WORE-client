88e1bc6a10f1f39a30dbc3b185b069d8f086c6ea
Kileer20100 <csgocvadimkiril80@gmail.com>
2025-10-19 16:00:11 +0200

    construction Chat

--- src/App.vue
+++ src/App.vue
@@ -9,24 +9,38 @@ const value = ref(null);
 </script>
 
 <template>
+<div class="messanger">
+  <div class="chat-list"></div>
+  <div class="chat">
+    <div class="card flex justify-center">
 
-<div class="card flex justify-center">
-  <IftaLabel>
-    <InputText id="message" v-model="value" />
-  </IftaLabel>
+      <IftaLabel>
+        <InputText id="message" v-model="value" />
+      </IftaLabel>
 
-  <Button label="Send" />
-
-  
-  
+      <Button label="Send" />
+    </div>
+  </div>
 </div>
-
-
 
 </template>
 
 
 <style>
+.messanger {
+  display: flex;
+  height: 100vh;
+}
+.chat-list {
+  flex: 4;
+  border: 1px solid #ccc;
+  overflow-y: auto;
+}
+.chat {
+  flex: 6;
+  border: 1px solid #ccc;
+  overflow-y: auto;
 
+}
 
 </style>
